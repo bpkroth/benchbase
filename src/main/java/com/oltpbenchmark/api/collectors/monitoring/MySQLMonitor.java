7d7ba8f5cd0c6d23fa5dd8fdf000fa7c46dd6364
Kelly Kostopoulou <kelkost@yahoo.gr>
2024-07-18 20:56:49 +0000

    added more metrics

--- src/main/java/com/oltpbenchmark/api/collectors/monitoring/MySQLMonitor.java
+++ src/main/java/com/oltpbenchmark/api/collectors/monitoring/MySQLMonitor.java
@@ -14,10 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Implementation of a monitor specific to PostgreSQL. Uses the 'pg_stat_statements' add-on to
- * extract relevant query and system information.
- */
 public class MySQLMonitor extends DatabaseMonitor {
 
   // TODO: add support for per-query metrics using performance_schema
@@ -37,11 +33,39 @@ public class MySQLMonitor extends DatabaseMonitor {
     this.repeatedSystemProperties =
         new ArrayList<String>() {
           {
-            add("bytes_received");
-            add("bytes_sent");
-            add("com_select");
-            // ...
-            // TODO: Add more properties from SHOW STATUS here
+            add("Connections");
+            add("Threads_running");
+            add("Threads_connected");
+            add("Handler_write");
+            add("Handler_commit");
+            add("Handler_update");
+            add("Handler_delete");
+            add("Handler_read_next");
+            add("Com_select");
+            add("Com_insert");
+            add("Com_insert_select");
+            add("Com_update");
+            add("Com_delete");
+            add("Select_full_join");
+            add("Select_full_range_join");
+            add("Select_range");
+            add("Select_range_check");
+            add("Select_scan");
+            add("Innodb_data_reads");
+            add("Innodb_data_writes");
+            add("Innodb_data_read");
+            add("Innodb_data_written");
+            add("Innodb_rows_deleted");
+            add("Innodb_rows_inserted");
+            add("Innodb_rows_read");
+            add("Innodb_rows_updated");
+            add("Innodb_buffer_pool_read_requests");
+            add("Sort_merge_passes");
+            add("Sort_range");
+            add("Sort_rows");
+            add("Sort_scan");
+            add("Bytes_received");
+            add("Bytes_sent");
           }
         };
   }
